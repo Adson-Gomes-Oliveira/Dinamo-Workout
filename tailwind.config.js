8c3eca709ec9dca7ef8e995428b3a9e4b4f0ad62
Adson Gomes Oliveira <agdev@DESKTOP-0N655A3.localdomain>
2022-08-19 21:33:40 -0300

    <settings/setup>: Initial setting and setup made

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    colors: {},
+    fontFamily: {},
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+}
